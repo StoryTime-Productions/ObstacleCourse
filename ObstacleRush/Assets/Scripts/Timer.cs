cfabc77aac5504e46913edc09d65a76c2a605b77
Nirav Patel <niravp0703@gmail.com>
2024-05-03 23:25:23 -0400

    fix: limited framerate to 60 fps
    This commit limits the framerate of the game application to 60 frames per second.

--- ObstacleRush/Assets/Scripts/Timer.cs
+++ ObstacleRush/Assets/Scripts/Timer.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public int frameCap;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+
     float time = 0;
     // Update is called once per frame
     void Update()
